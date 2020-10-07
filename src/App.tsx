a05e3599cdf03f320dccb3fd4d51d87a8030bc28
jairorudas <jairo@gavea.com>
2020-10-07 16:04:32 -0300

    iplemented root navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import AplicationProvider from "./context/aplicationContext";
 import Menu from "./components/menu";
 import Keyboard from "./components/keyboard";
+import Navigation from "./utils/navContainer";
 
 import Container from "./components/container";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [activeContainer, setActiveContainer] = useState(1);
+
   return (
     <AplicationProvider>
-      <Menu />
+      <Menu
+        isActive={activeContainer === Navigation.menu}
+        onLeaveRight={() => setActiveContainer(Navigation.keyboard)}
+      />
       <section className="main-content">
         <div className="App" style={{ overflow: "hidden" }}>
-          <Keyboard />
+          <Keyboard
+            isActive={activeContainer === Navigation.keyboard}
+            onLeaveLeft={() => setActiveContainer(Navigation.menu)}
+            onLeaveRight={() => setActiveContainer(Navigation.carousel1)}
+          />
           <section className="filmes-content">
             <h4 className="label-carousel">Filmes</h4>
-            <Container id={2}></Container>
+            <Container
+              id={2}
+              isActive={activeContainer === Navigation.carousel1}
+              onLeaveLeft={() => setActiveContainer(Navigation.keyboard)}
+              onLeaveDown={() => setActiveContainer(Navigation.carousel2)}
+            ></Container>
+
             <h4 className="label-carousel">Series</h4>
-            <Container id={3}></Container>
+            <Container
+              id={3}
+              isActive={activeContainer === Navigation.carousel2}
+              onLeaveLeft={() => setActiveContainer(Navigation.keyboard)}
+              onLeaveTop={() => setActiveContainer(Navigation.carousel1)}
+            ></Container>
           </section>
         </div>
       </section>
